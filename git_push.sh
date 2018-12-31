18f7f24f21e0907a34727933f9cc96214f8ee0a1
Yaron Amir <yaron.a@finscend.com>
2018-12-31 14:04:11 +0200

    cached git_push

--- git_push.sh
+++
@@ -1,8 +0,0 @@
-if [ -n "$1" ]; then
-	git add .
-	git commit -m "$1"
-	git push https://yaron.a%40finscend.com@github.com/YaroniAmir/git-test.git
-else
-	echo "missing commit comment! Please enter coomit comment."
-fi
-
