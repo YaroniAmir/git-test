461bf37a72fe1d43b51770e852381aa06412a63c
Yaron Amir <yaron.a@finscend.com>
2018-12-31 11:41:29 +0200

    test3

--- git_push.sh
+++ git_push.sh
@@ -1,3 +1,8 @@
-git add .
-git commit -m "$1"
-git push https://yaron.a%40finscend.com@github.com/YaroniAmir/git-test.git origin master
+if [ -n "$1" ]; then
+	git add .
+	git commit -m "$1"
+	git push https://yaron.a%40finscend.com@github.com/YaroniAmir/git-test.git origin master
+else
+	echo "missing commit comment"
+fi
+
